fea38398b167a45e575ad8865f4918e029e71c8b
Olga Taranukha <o.taranukha.work@gmail.com>
2025-09-26 16:22:00 +0400

    changed configs

--- my-personal-portfolio/docusaurus.config.js
+++ my-personal-portfolio/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  url: 'https://slusimusi.github.io',
+  baseUrl: '/portfolio/', 
+  organizationName: 'slusimusi', // Usually your GitHub org/user name.
+  projectName: 'portfolio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
