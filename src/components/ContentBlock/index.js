ec748db649abb567853e064eb5a7248b85c6a530
Olga Taranukha <o.taranukha.work@gmail.com>
2025-11-24 08:11:38 +0400

    fixed img loading

--- src/components/ContentBlock/index.js
+++ src/components/ContentBlock/index.js
@@ -2,9 +2,9 @@ import styles from './styles.module.css';
 
 export default function ContentBlock({text, alt, source}) {
   return (
-    <div class={styles.flexContainer}>
+    <div className={styles.flexContainer}>
         <p>{text}</p>
-        <img alt={alt} src={source}/>
+        <img alt={alt} src={require(`@site/static${source}`).default}/>
     </div>
   );
 }
